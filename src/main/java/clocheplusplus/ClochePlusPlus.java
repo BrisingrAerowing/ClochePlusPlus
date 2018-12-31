0f0b79ed7f5e8fb56fb6b82c1cbabbca11812907
Brisingr Aerowing <ztgreve@live.com>
2018-12-31 18:39:47 -0500

    Bump version

--- src/main/java/clocheplusplus/ClochePlusPlus.java
+++ src/main/java/clocheplusplus/ClochePlusPlus.java
@@ -13,7 +13,7 @@ public class ClochePlusPlus {
 
 	public static final String MODID = "clocheplusplus";
 	public static final String NAME = "Cloche++";
-	public static final String VERSION = "0.4";
+	public static final String VERSION = "0.5";
 	public static final String DEPENDENCIES="required-after:immersiveengineering;after:weeeflowers;after:t3s4ebw";
 
 	@EventHandler
